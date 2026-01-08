f0fbe96f145c220aa034d9e7c74f2498f573f2f1
agentpppp <bondsarthakpbt@gmail.com>
2026-01-08 12:34:47 +0530

    file correction

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { db } from '@/lib/firebase'; 
+import { db } from '@/lib/authentication /signIn'
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 import { getAIQuizAdvice } from './Interface';
 
